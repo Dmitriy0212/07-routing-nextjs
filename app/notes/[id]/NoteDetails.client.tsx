b8065bb97b0577221c4b45f7dd4506da64b818fb
Dmitriy0212 <demarr68@gmail.com>
2026-05-31 16:09:57 +0300

    fix

--- app/notes/[id]/NoteDetails.client.tsx
+++ app/notes/[id]/NoteDetails.client.tsx
@@ -1,29 +1,31 @@
 'use client';
-
 import css from './NoteDetails.module.css';
+import { useParams } from 'next/navigation';
+import { useQuery } from '@tanstack/react-query';
+import { getSingleNote } from '@/lib/api';
 
-type Note = {
-  title: string;
-  tag: string;
-  content: string;
-  createdAt: string;
-};
+export default function NoteDetailsClient() {
+  const { id } = useParams<{ id: string }>();
 
-type Props = {
-  note: Note;
-};
+  const { data, isLoading, error } = useQuery({
+    queryKey: ['note', id],
+    queryFn: () => getSingleNote(id),
+    refetchOnMount: false,
+  });
 
-export default function NoteDetailsClient({ note }: Props) {
+  if (isLoading) return <p>Loading...</p>;
+  if (error) return <p>Something went wrong.</p>;
+
   return (
     <div className={css.container}>
       <div className={css.item}>
         <div className={css.header}>
-          <h2>{note.title}</h2>
+          <h2>{data.title}</h2>
         </div>
 
-        <p className={css.tag}>{note.tag}</p>
-        <p className={css.content}>{note.content}</p>
-        <p className={css.date}>{note.createdAt}</p>
+        <p className={css.tag}>{data.tag}</p>
+        <p className={css.content}>{data.content}</p>
+        <p className={css.date}>{data.createdAt}</p>
       </div>
     </div>
   );
